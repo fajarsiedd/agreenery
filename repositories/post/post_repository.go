186cab222b2b200abc7d4313ccc7bdefcf5d0d72
Fajar Sidiq <fajarsiedd@gmail.com>
2024-12-09 19:49:15 +0700

    ref: params name

--- repositories/post/post_repository.go
+++ repositories/post/post_repository.go
@@ -5,7 +5,7 @@ import "go-agreenery/entities"
 type PostRepository interface {
 	GetPosts(filter entities.Filter) ([]entities.Post, entities.Pagination, error)
 	GetPost(id, userID string) (entities.Post, error)
-	CreatePost(category entities.Post) (entities.Post, error)
-	UpdatePost(category entities.Post, currUserID string) (entities.Post, error)
+	CreatePost(post entities.Post) (entities.Post, error)
+	UpdatePost(post entities.Post, currUserID string) (entities.Post, error)
 	DeletePost(id, currUserID string) (string, error)
 }
